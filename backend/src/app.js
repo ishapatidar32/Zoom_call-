7cbbce2e3e009c8e5c68ba20280c732babd6b460
isha patidar <patidarisha027@gmail.com>
2026-07-12 17:21:22 +0530

    backend update

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,18 +8,29 @@ import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
 
 import cors from "cors";
+import  helmet from "helmet";
+import rateLimit from "express-rate-limit";
 import userRoutes from "./routes/users.routes.js";
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
+app.use(helmet());
 
+
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: "Too many attempts, try again later."
+});
 
 app.set("port", (process.env.PORT || 8000))
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 app.use("/api/v1/users", userRoutes);
-
+app.use("/api/v1/users/login", authLimiter);
+app.use("/api/v1/users/register", authLimiter);
 
 const start = async () => {
      app.set("mongo_user")
